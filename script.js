d4406b88714bcb5713c2fe8b95ce9ff20db9565c
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-08-12 13:37:28 -0600

    basic logic complete

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ function printOutput(num) {
 } 
 
 function getFormattedNumber(num) {
+  if (num === "-") {
+    return "";
+  }
   let n = Number(num);
   let value = n.toLocaleString("en");
   return value;
@@ -31,14 +34,43 @@ function reverseNumberFormat(num) {
 let operator = document.getElementsByClassName("operator");
 for(let i=0; i<operator.length; i++) {
   operator[i].addEventListener('click', function() {
-    // console.log("The operator clicked: " + this.id)
-  })
+    if (this.id === "clear") {
+      printHistory("")
+      printOutput("")
+    } else if (this.id === "backspace") {
+      let output = reverseNumberFormat(getOutput()).toString();
+      if(output) {
+        output = output.substr(0, output.length-1);
+        printOutput(output);
+      }
+    } else {
+      let output = getOutput();
+      let history = getHistory();
+      if (output !== "") {
+        output = reverseNumberFormat(output);
+        history = history + output;
+        if (this.id === "=") {
+          let result = eval(history);
+          printOutput(result);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput("");
+        }
+      }
+    }
+  });
 }
 
 let number = document.getElementsByClassName("number");
 for(let i=0; i<number.length; i++) {
   number[i].addEventListener('click', function() {
-    console.log("The number clicked: " + this.id)
-  })
+    let output = reverseNumberFormat(getOutput());
+    if (output !== NaN) {
+      output = output + this.id;
+      printOutput(output)
+    }
+  });
 }
 
